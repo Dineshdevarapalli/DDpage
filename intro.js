456373b53f3d77e0e830ec49f02d019176641e1e
Dineshdevarapalli <92310861+Dineshdevarapalli@users.noreply.github.com>
2023-04-14 22:35:08 +0530

    Initial commit

---
+++ intro.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navlinks = document.querySelector('.nav-links')
+menuBtn.addEventListener('click',()=>{
+// navlinks.classList.toggle('mobile-menu')
+
+navlinks.classList.toggle("mobile-menu");
+})
+// menuBtn.addEventListener('click',()=>{
+// navlinks.classList.remove("mobile-menu");
+// })
